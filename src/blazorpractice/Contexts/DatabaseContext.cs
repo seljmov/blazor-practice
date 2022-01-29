5f37cdbdef2bc347e2f49a358f19b86c37fb9010
Zagir Selimov <seljmov@list.ru>
2022-01-29 23:53:09 +0400

    Реализован контекст базы данных

---
+++ src/blazorpractice/Contexts/DatabaseContext.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using blazorpractice.Models;
+
+namespace blazorpractice.Contexts;
+
+/// <summary>
+/// Контекст базы данных
+/// </summary>
+public class DatabaseContext : DbContext
+{
+    private readonly string _databasePath = @"Server=(localdb)\\mssqllocaldb;Database=blazorpracticedb;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_databasePath);
+    }
+
+    /// <summary>
+    /// Словарь предприятий
+    /// </summary>
+    public DbSet<Company> Companies { get; set; }
+
+    /// <summary>
+    /// Словарь связей предприятие/владелец
+    /// </summary>
+    public DbSet<CompanyOwnerRelations> CompanyOwnerRelations { get; set; }
+
+    /// <summary>
+    /// Словарь связей предприятие/продукт
+    /// </summary>
+    public DbSet<CompanyProductRelations> CompanyProductRelations { get; set; }
+
+    /// <summary>
+    /// СЛоварь отраслей экономики
+    /// </summary>
+    public DbSet<EconomicSector> EconomicSectors { get; set; }
+
+    /// <summary>
+    /// Словарь ингредиентов продуктов
+    /// </summary>
+    public DbSet<Ingredient> Ingredients { get; set; }
+
+    /// <summary>
+    /// Словарь местоположений
+    /// </summary>
+    public DbSet<Location> Locations { get; set; }
+
+    /// <summary>
+    /// Словарь логов
+    /// </summary>
+    public DbSet<LogInfo> LogInfos { get; set; }
+
+    /// <summary>
+    /// Словарь владельцев
+    /// </summary>
+    public DbSet<Owner> Owners { get; set; }
+
+    /// <summary>
+    /// Словарь форм собственности
+    /// </summary>
+    public DbSet<OwnershipForm> OwnershipForms { get; set; }
+
+    /// <summary>
+    /// Словарь предприятий-партнеров
+    /// </summary>
+    public DbSet<Partner> Partners { get; set; }
+
+    /// <summary>
+    /// Словарь продуктов предприятий
+    /// </summary>
+    public DbSet<Product> Products { get; set; }
+
+    /// <summary>
+    /// Словарь связей продукт/ингредиент
+    /// </summary>
+    public DbSet<ProductIngredientRelations> ProductIngredientRelations { get; set; }
+
+    /// <summary>
+    /// Словарь предприятий-конкурентов
+    /// </summary>
+    public DbSet<Rival> Rivals { get; set; }
+
+    /// <summary>
+    /// Словарь целевых направлений
+    /// </summary>
+    public DbSet<TargetPurpose> TargetPurposes { get; set; }
+}
---
+++ src/blazorpractice/Models/LogInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace blazorpractice.Models;
+
+/// <summary>
+/// Информация для лога
+/// </summary>
+public class LogInfo
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public int Id {  get; set; }
+
+    /// <summary>
+    /// Информация
+    /// </summary>
+    public string Info { get; set; }
+
+    /// <summary>
+    /// Дата создания лога
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+}
