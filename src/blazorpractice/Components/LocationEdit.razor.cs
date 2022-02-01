19aab7ef73ff0684efd91e3a6530e5a177878ef8
Zagir Selimov <seljmov@list.ru>
2022-02-01 22:57:55 +0400

    Добавлена логика для работы с местоположениями

--- src/blazorpractice/Components/LocationEdit.razor.cs
+++ src/blazorpractice/Components/LocationEdit.razor.cs
@@ -6,15 +6,35 @@ namespace blazorpractice.Components;
 
 public partial class LocationEdit
 {
-    private readonly DatabaseContext _context = new DatabaseContext();
+    private readonly DatabaseContext _context = new();
 
     [Parameter]
     public int Id { get; set; }
 
     private Location Location { get; set; }
 
+    private bool EndEdit { get; set; } = false;
+    private bool SuccessEdit { get; set; } = false;
+
     protected override void OnInitialized()
     {
         Location = _context.Locations.ToList().FirstOrDefault(location => location.Id == Id);
+    }
+
+    private void EditLocation()
+    {
+        EndEdit = true;
+        try
+        {
+            Location.Edit();
+            SuccessEdit = true;
+            return;
+        }
+        catch (Exception)
+        {
+
+        }
+
+        SuccessEdit = false;
     }
 }
