62552e15a386be40d20a7288e3c81fec5aceea6f
Zagir Selimov <seljmov@list.ru>
2022-02-02 23:34:44 +0400

    Исправления в модели

--- src/blazorpractice/Models/TargetPurpose.cs
+++ src/blazorpractice/Models/TargetPurpose.cs
@@ -3,30 +3,11 @@
 /// <summary>
 /// Целевое назначение
 /// </summary>
-public class TargetPurpose : IHandbookBase, INameable
+public class TargetPurpose : IReadOnlyHandbook, INameable
 {
     /// <inheritdoc cref="IReadOnlyHandbook.Id"/> 
     public int Id { get; set; }
 
     /// <inheritdoc cref="INameable.Name"/> 
     public string Name { get; set; }
-
-
-    /// <inheritdoc cref="IHandbookBase.Create"/> 
-    public void Create()
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <inheritdoc cref="IHandbookBase.Edit"/> 
-    public void Edit()
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <inheritdoc cref="IHandbookBase.Remove"/> 
-    public void Remove()
-    {
-        throw new NotImplementedException();
-    }
 }
